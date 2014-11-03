6e7fc472e3956da211402b1dac004672938f6041
jonathanCohen8 <jonathan_cohen@outlook.com>
2014-11-03 15:35:40 +0100

    Méthode de tests

--- src/magicsquares/MagicSquares.java
+++ src/magicsquares/MagicSquares.java
@@ -1,15 +1,18 @@
 package magicsquares;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintStream;
+import java.util.ArrayList;
+
 import solver.Solver;
 import solver.constraints.IntConstraintFactory;
-import solver.exception.ContradictionException;
-import solver.search.strategy.decision.Decision;
-import solver.search.strategy.strategy.AbstractStrategy;
 import solver.variables.IntVar;
 import solver.variables.VariableFactory;
 import util.tools.ArrayUtils;
 
 public class MagicSquares {
+    
     public static interface SolverFactory {
         public abstract Solver build();
     }
@@ -20,6 +23,10 @@ public class MagicSquares {
         }
     }
 
+    
+    //
+    // Résolution d'un carré magique normal d'ordre n
+    //
     public static IntVar[][] solveMagicSquare(int n, SolverFactory solverFactory) {
         Solver solver = solverFactory.build();
         IntVar sum = VariableFactory.fixed((n * (n * n + 1)) / 2, solver);
@@ -47,14 +54,11 @@ public class MagicSquares {
 
         return vs;
     }
-
-    public static void main(String[] args) {
-        final int n = 6;
-        IntVar[][] solution = solveMagicSquare(n, new BasicSolverFactory());
-        displaySolution(solution);
-    }
-
-    // Affichage d'une solution trouvee
+    
+    
+    //
+    // Affichage d'une solution trouvée
+    //
     public static void displaySolution(IntVar[][] solution) {
         for(int i = 0; i < solution.length; i++) {
             for(int j = 0; j < solution[i].length; j++) {
@@ -63,5 +67,38 @@ public class MagicSquares {
             }
             System.out.println();
         }
+    }
+    
+    
+    //
+    // Tests automatiques 
+    //
+    // Écrit un tableau avec la taille du carré et le temps de résolution associé
+    public static void test(int n, PrintStream ps) {
+        for(int i = 1; i <= n; i++) {
+            double start = System.currentTimeMillis();
+            solveMagicSquare(i, new BasicSolverFactory());
+            double end = System.currentTimeMillis();
+            ps.println(i + "\t" + (end - start));
+        }
+    }
+    
+
+    //
+    // Main
+    //
+    public static void main(String[] args) {
+        
+        final int n = 6;
+        /*String xp_name = "xp_1";
+        try {
+            File f = new File("C:\\Users\\Jonathan\\SkyDrive\\Université\\M2\\Résolution de problèmes combinatoires\\Projet\\" + xp_name);
+            PrintStream ps = new PrintStream(f);
+            test(n, ps);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }*/
+        test(n, System.out);
+        
     }
 }
