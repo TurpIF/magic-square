dc12186c99225551a1c203e5d509bd862e5caa23
TurpIF <pierre.turpin@insa-lyon.fr>
2014-11-03 16:27:09 +0100

    Plein de stratégies

--- src/magicsquares/MagicSquares.java
+++ src/magicsquares/MagicSquares.java
@@ -13,8 +13,11 @@ import solver.search.strategy.decision.Decision;
 import solver.search.strategy.selectors.VariableSelector;
 import solver.search.strategy.selectors.VariableSelectorWithTies;
 import solver.search.strategy.selectors.values.IntDomainMax;
+import solver.search.strategy.selectors.values.IntDomainMiddle;
 import solver.search.strategy.selectors.values.IntDomainMin;
+import solver.search.strategy.selectors.values.IntDomainRandom;
 import solver.search.strategy.selectors.variables.FirstFail;
+import solver.search.strategy.selectors.variables.Largest;
 import solver.search.strategy.selectors.variables.Random;
 import solver.search.strategy.selectors.variables.Smallest;
 import solver.search.strategy.strategy.AbstractStrategy;
@@ -38,9 +41,75 @@ public class MagicSquares {
         }
     }
 
-    public static class BasicStrategyFactory extends StrategyFactory {
+    public static class SmallestIntDomainMaxStrategyFactory extends StrategyFactory {
         public AbstractStrategy build(IntVar[][] vars) {
             return ISF.custom(new VariableSelectorWithTies<IntVar>(new Smallest()), new IntDomainMax(), ArrayUtils.flatten(vars));
+        }
+    }
+
+    public static class SmallestIntDomainMinStrategyFactory extends StrategyFactory {
+        public AbstractStrategy build(IntVar[][] vars) {
+            return ISF.custom(new VariableSelectorWithTies<IntVar>(new Smallest()), new IntDomainMin(), ArrayUtils.flatten(vars));
+        }
+    }
+
+    public static class SmallestIntDomainMiddleStrategyFactory extends StrategyFactory {
+        public AbstractStrategy build(IntVar[][] vars) {
+            return ISF.custom(new VariableSelectorWithTies<IntVar>(new Smallest()), new IntDomainMiddle(), ArrayUtils.flatten(vars));
+        }
+    }
+
+    public static class SmallestIntDomainRandomStrategyFactory extends StrategyFactory {
+        public AbstractStrategy build(IntVar[][] vars) {
+            return ISF.custom(new VariableSelectorWithTies<IntVar>(new Smallest()), new IntDomainRandom(42L), ArrayUtils.flatten(vars));
+        }
+    }
+
+    public static class LargestIntDomainMaxStrategyFactory extends StrategyFactory {
+        public AbstractStrategy build(IntVar[][] vars) {
+            return ISF.custom(new VariableSelectorWithTies<IntVar>(new Largest()), new IntDomainMax(), ArrayUtils.flatten(vars));
+        }
+    }
+
+    public static class LargestIntDomainMinStrategyFactory extends StrategyFactory {
+        public AbstractStrategy build(IntVar[][] vars) {
+            return ISF.custom(new VariableSelectorWithTies<IntVar>(new Largest()), new IntDomainMin(), ArrayUtils.flatten(vars));
+        }
+    }
+
+    public static class LargestIntDomainMiddleStrategyFactory extends StrategyFactory {
+        public AbstractStrategy build(IntVar[][] vars) {
+            return ISF.custom(new VariableSelectorWithTies<IntVar>(new Largest()), new IntDomainMiddle(), ArrayUtils.flatten(vars));
+        }
+    }
+
+    public static class LargestIntDomainRandomStrategyFactory extends StrategyFactory {
+        public AbstractStrategy build(IntVar[][] vars) {
+            return ISF.custom(new VariableSelectorWithTies<IntVar>(new Largest()), new IntDomainRandom(42L), ArrayUtils.flatten(vars));
+        }
+    }
+
+    public static class RandomIntDomainMaxStrategyFactory extends StrategyFactory {
+        public AbstractStrategy build(IntVar[][] vars) {
+            return ISF.custom(new VariableSelectorWithTies<IntVar>(new Random<IntVar>(42L)), new IntDomainMax(), ArrayUtils.flatten(vars));
+        }
+    }
+
+    public static class RandomIntDomainMinStrategyFactory extends StrategyFactory {
+        public AbstractStrategy build(IntVar[][] vars) {
+            return ISF.custom(new VariableSelectorWithTies<IntVar>(new Random<IntVar>(42L)), new IntDomainMin(), ArrayUtils.flatten(vars));
+        }
+    }
+
+    public static class RandomIntDomainMiddleStrategyFactory extends StrategyFactory {
+        public AbstractStrategy build(IntVar[][] vars) {
+            return ISF.custom(new VariableSelectorWithTies<IntVar>(new Random<IntVar>(42L)), new IntDomainMiddle(), ArrayUtils.flatten(vars));
+        }
+    }
+
+    public static class RandomIntDomainRandomStrategyFactory extends StrategyFactory {
+        public AbstractStrategy build(IntVar[][] vars) {
+            return ISF.custom(new VariableSelectorWithTies<IntVar>(new Random<IntVar>(42L)), new IntDomainRandom(42L), ArrayUtils.flatten(vars));
         }
     }
 
@@ -96,6 +165,12 @@ public class MagicSquares {
     //
     // Écrit un tableau avec la taille du carré et le temps de résolution associé
     public static void test(int n, PrintStream ps, StrategyFactory... strategies) {
+        ps.print("Strategy");
+        for (int i = 1; i <= n; i++) {
+            ps.print("\tn=" + i);
+        }
+        ps.println();
+
         for (StrategyFactory strategy : strategies) {
             ps.print(strategy.toString());
             for (int i = 1; i <= n; i++) {
@@ -113,7 +188,6 @@ public class MagicSquares {
     // Main
     //
     public static void main(String[] args) {
-        
         final int n = 6;
         /*String xp_name = "xp_1";
         try {
@@ -123,7 +197,20 @@ public class MagicSquares {
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }*/
-        test(n, System.out, new DefaultStrategyFactory());
+        test(n, System.out,
+                new DefaultStrategyFactory(),
+                new LargestIntDomainMaxStrategyFactory(),
+                new LargestIntDomainMiddleStrategyFactory(),
+                new LargestIntDomainMinStrategyFactory(),
+                new LargestIntDomainRandomStrategyFactory(),
+                new SmallestIntDomainMaxStrategyFactory(),
+                new SmallestIntDomainMiddleStrategyFactory(),
+                new SmallestIntDomainMinStrategyFactory(),
+                new SmallestIntDomainRandomStrategyFactory(),
+                new RandomIntDomainMaxStrategyFactory(),
+                new RandomIntDomainMiddleStrategyFactory(),
+                new RandomIntDomainMinStrategyFactory(),
+                new RandomIntDomainRandomStrategyFactory());
         
     }
 }
